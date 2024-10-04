946846792907a39273d7ecf74dc80d9412eb507a
ohampro <mohammad.haghighi.poor@gmail.com>
2024-10-04 10:47:57 +0300

    Refactor: Improve tests structure

---
+++ test/helpers/my_app.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+import 'package:theme_provider/theme_provider.dart';
+import 'package:theme_provider/theme_service.dart';
+import 'package:theme_provider/themes.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ThemeProvider<ThemeData>(
+      themes: const DefaultMaterialAppThemes(),
+      builder: (theme, darkTheme){
+        return MaterialApp(
+          title: 'Material Test',
+          theme: theme,
+          darkTheme: darkTheme,
+          home: const MyHomePage(),
+        );
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+const changeThemeButtonKey = ValueKey('changeThemeButtonKey');
+const nextModeButtonKey = ValueKey('nextModeButtonKey');
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+            key: changeThemeButtonKey, 
+            onPressed: ThemeProvider.of(context).toggle, 
+            child: const Text('Change Theme'),
+          ),
+          Text(Theme.of(context).brightness.toString()),
+
+          ElevatedButton(
+            key: nextModeButtonKey, 
+            onPressed: ThemeProvider.of(context).next, 
+            child: const Text('Next Mode'),
+          ),
+          Text('Mode: ${ThemeProvider.of(context).mode.toString()}'),
+        ],
+      ),
+    );
+  }
+}
+
+class ThemeServiceTestClass with ThemeService{}
--- test/theme_provider_test.dart
+++ test/theme_provider_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:theme_provider/theme_provider.dart';
-import 'package:theme_provider/theme_service.dart';
-import 'package:theme_provider/themes.dart';
+import 'helpers/my_app.dart';
 
 void main() {
   group('ThemeService', (){
@@ -95,60 +93,4 @@ void main() {
       expect(find.text('Mode: ${ThemeMode.light.toString()}'), findsOneWidget);
     });
   });
-}
-
-class ThemeServiceTestClass with ThemeService{}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ThemeProvider<ThemeData>(
-      themes: const DefaultMaterialAppThemes(),
-      builder: (theme, darkTheme){
-        return MaterialApp(
-          title: 'Material Test',
-          theme: theme,
-          darkTheme: darkTheme,
-          home: const MyHomePage(),
-        );
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-const changeThemeButtonKey = ValueKey('changeThemeButtonKey');
-const nextModeButtonKey = ValueKey('nextModeButtonKey');
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
-            key: changeThemeButtonKey, 
-            onPressed: ThemeProvider.of(context).toggle, 
-            child: const Text('Change Theme'),
-          ),
-          Text(Theme.of(context).brightness.toString()),
-
-          ElevatedButton(
-            key: nextModeButtonKey, 
-            onPressed: ThemeProvider.of(context).next, 
-            child: const Text('Next Mode'),
-          ),
-          Text('Mode: ${ThemeProvider.of(context).mode.toString()}'),
-        ],
-      ),
-    );
-  }
 }
