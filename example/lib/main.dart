1842667940f6716715ad6f3bed68f0b11a710784
ohampro <mohammad.haghighi.poor@gmail.com>
2024-10-04 22:27:52 +0300

    Update Example app to show Cupertino Theme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:x_theme_provider/theme_provider.dart';
+
+bool material = true;
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +15,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ThemeProvider(
       builder: (mode){
-          return MaterialApp(
-          title: 'Flutter Demo',
-          theme: DefaultMaterialTheme.of(mode),
-          darkTheme: DefaultMaterialTheme.dark,
-          home: const MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-      },
+          return material ? 
+          MaterialApp(
+            title: 'Flutter Demo',
+            theme: DefaultMaterialTheme.of(mode),
+            darkTheme: DefaultMaterialTheme.dark,
+            home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          ) : 
+          CupertinoApp(
+            theme: DefaultCupertinoTheme.of(mode),
+            home: const MyCupertinoHomePage(title: 'Flutter Demo Home Page'),
+          );
+        },
     );
   }
 }
@@ -80,6 +88,15 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             const SizedBox(height: 50,),
+            ElevatedButton.icon(
+              onPressed: () {
+                material = !material;
+                ThemeProvider.of(context).next();
+              }, 
+              icon: Icon(material ? Icons.apple : Icons.android), 
+              label: Text(material ? 'Cupertino' : 'Material'),
+            ),
+            const SizedBox(height: 50,),
             Text('${ThemeProvider.of(context).mode} Mode'),
           ],
         ),
@@ -88,6 +105,134 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: ThemeProvider.of(context).toggle,
         tooltip: 'toggle',
         child: const Icon(Icons.contrast),
+      ),
+    );
+  }
+}
+
+
+// ------------------------------- Cupertino App
+
+class MyCupertinoHomePage extends StatefulWidget {
+  const MyCupertinoHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyCupertinoHomePage> createState() => _MyCupertinoHomePageState();
+}
+
+class _MyCupertinoHomePageState extends State<MyCupertinoHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('Cupertino Theme'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  child: CupertinoButton(
+                    onPressed: ThemeProvider.of(context).light, 
+                    child: const Row(
+                      children: [
+                        Icon(Icons.light_mode),
+                        Text('light'),
+                      ],
+                    ), 
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: CupertinoButton(
+                    onPressed: ThemeProvider.of(context).dark, 
+                    child: const Row(
+                      children: [
+                        Icon(Icons.dark_mode),
+                        Text('dark'),
+                      ],
+                    ), 
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: CupertinoButton(
+                    onPressed: ThemeProvider.of(context).system, 
+                    child: const Row(
+                      children: [
+                        Icon(Icons.settings), 
+                        Text('system'),
+                      ],
+                    ), 
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: CupertinoButton(
+                    onPressed: ThemeProvider.of(context).toggle, 
+                    child: Row(
+                      children: [
+                        Icon(ThemeProvider.of(context).isDark ? Icons.toggle_off : Icons.toggle_on), 
+                        const Text('toggle'),
+                      ],
+                    ), 
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: CupertinoButton(
+                    onPressed: ThemeProvider.of(context).previous, 
+                    child: const Row(
+                      children: [
+                        Icon(Icons.skip_previous), 
+                        Text('previous'),
+                      ],
+                    ), 
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: CupertinoButton(
+                    onPressed: ThemeProvider.of(context).next, 
+                    child: const Row(
+                      children: [
+                        Icon(Icons.skip_next), 
+                        Text('next'),
+                      ],
+                    ), 
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50,),
+            SizedBox(
+              width: 150,
+              child: CupertinoButton(
+                onPressed: () {
+                  material = !material;
+                  ThemeProvider.of(context).previous();
+                }, 
+                child: Row(
+                  children: [
+                    Icon(material ? Icons.apple : Icons.android), 
+                    Text(material ? 'Cupertino' : 'Material'),
+                  ],
+                ), 
+              ),
+            ),
+            const SizedBox(height: 50,),
+            Text('${ThemeProvider.of(context).mode} Mode'),
+          ],
+        ),
       ),
     );
   }
